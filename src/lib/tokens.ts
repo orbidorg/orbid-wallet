0b552b9b83dd21f0c7ec0b0ebb95f33efb689306
orbidlabs <orbidorg@gmail.com>
2025-12-21 04:56:58 -0500

    feat: add World Chain Mainnet token testing with ORO contract

--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -94,11 +94,18 @@ export const WORLD_CHAIN_TOKENS: Token[] = [
         decimals: 18,
         logoURI: 'https://assets.coingecko.com/coins/images/29850/standard/pepe-token.jpeg',
     },
+    {
+        symbol: 'OID',
+        name: 'OrbID Token',
+        address: '0xCd1E32b86953d79a6aC58e813d2eA7A1790CAB63',
+        decimals: 18,
+        logoURI: 'https://magenta-wrong-impala-427.mypinata.cloud/ipfs/bafkreibwb3ywvgfi3j2zh2ywa4ano47cvm5gwlp2batrizolk6wqn7oogy',
+    },
     // TODO: Uncomment when added to developer portal
     //{
     //    symbol: 'OID',
     //    name: 'OrbID Token',
-    //    address: '0x0000000000000000000000000000000000000000',
+    //    address: '0xCd1E32b86953d79a6aC58e813d2eA7A1790CAB63',
     //    decimals: 18,
     //    logoURI: 'https://magenta-wrong-impala-427.mypinata.cloud/ipfs/bafkreibwb3ywvgfi3j2zh2ywa4ano47cvm5gwlp2batrizolk6wqn7oogy',
     //},
